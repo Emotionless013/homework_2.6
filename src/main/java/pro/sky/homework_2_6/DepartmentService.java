966899b3c6ebde620a3af3c703209c978aae4df6
Alexander <emotionless013@mail.ru>
2022-09-18 20:03:59 +0400

    hw2.8

--- src/main/java/pro/sky/homework_2_6/DepartmentService.java
+++ src/main/java/pro/sky/homework_2_6/DepartmentService.java
@@ -1,37 +1,27 @@
 package pro.sky.homework_2_6;
 
 import org.springframework.stereotype.Service;
+import pro.sky.homework_2_6.exeptions.EmployeeNotFoundException;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class DepartmentService extends EmployeeService {
     //Ищем сотрудников с минимальной ЗП в отделе
-    protected static List<Employee> minSalaryEmployeeDepartment(int departmentID) {
+    protected static Optional<Employee> minSalaryEmployeeDepartment(int departmentID) {
         List<Employee> workList = new ArrayList<>(employeeBook.values());
-        double salaryMin = workList.stream().filter(e -> e.getDepartmentID() == departmentID).
-                map(s -> s.getEmployeeSalary()).min(Comparator.naturalOrder()).
-                orElseThrow(NoSuchElementException::new);
-        return workList.stream().
+        return Optional.ofNullable(workList.stream().
                 filter(e -> e.getDepartmentID() == departmentID).
-                filter(employee -> employee.getEmployeeSalary() == salaryMin).
-                collect(Collectors.toList());
+                min(Comparator.comparing(Employee::getEmployeeSalary)).orElseThrow(EmployeeNotFoundException::new));
     }
 
     //Ищем сотрудников с максимальной ЗП в отделе
-    protected static List<Employee> maxSalaryEmployeeDepartment(int departmentID) {
+    protected static Optional<Employee> maxSalaryEmployeeDepartment(int departmentID) {
         List<Employee> workList = new ArrayList<>(employeeBook.values());
-        double salaryMax = workList.stream().filter(e -> e.getDepartmentID() == departmentID).
-                map(s -> s.getEmployeeSalary()).max(Comparator.naturalOrder()).
-                orElseThrow(NoSuchElementException::new);
-        return workList.stream().
+        return Optional.ofNullable(workList.stream().
                 filter(e -> e.getDepartmentID() == departmentID).
-                filter(employee -> employee.getEmployeeSalary() == salaryMax).
-                collect(Collectors.toList());
+                max(Comparator.comparing(Employee::getEmployeeSalary)).orElseThrow(EmployeeNotFoundException::new));
     }
 
     //выводим данные отрудников отдела
@@ -45,7 +35,7 @@ public class DepartmentService extends EmployeeService {
     protected static List<Employee> everyEmployeesDepartment() {
         List<Employee> workList = new ArrayList<>(employeeBook.values());
         return workList.stream().
-                sorted(Comparator.comparing(e -> e.getDepartmentID())).
+                sorted(Comparator.comparing(Employee::getDepartmentID)).
                 collect(Collectors.toList());
     }
 }
